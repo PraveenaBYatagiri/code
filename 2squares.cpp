7402adf6d8a790db4ba0d61624ce71998fdf8d3e
Praveena B Yatagiri <praveenabyatagiri@gmail.com>
2026-09-08 22:12:45 +0530

    square inside square

---
+++ 2squares.cpp
@@ -0,0 +1,12 @@
+#include <simplecpp>
+main_program{
+ turtleSim();
+ repeat(4){
+  forward(100);right(90);
+  wait(5);}
+ forward(50);right(45);wait(2);
+ repeat(4){
+  forward(sqrt(5000));right(90);
+  }
+ wait(5);
+}
